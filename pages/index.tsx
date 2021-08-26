9e5780abd6a4d891b7509849c644f28c339f584c
biels <bielsimon2@gmail.com>
2021-08-26 13:01:49 +0200

    Added bulma

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,5 +6,16 @@ const Title = styled.h1`
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>My page
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello World
+        </h1>
+        <p className="subtitle">
+          My first website with <strong>Bulma</strong>!
+        </p>
+      </div>
+    </section>
+  </Title>
 }
