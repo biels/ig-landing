08c6b766f6b3f9fe640611be2ca182d9b9a245df
biels <bielsimon2@gmail.com>
2021-09-11 12:21:26 +0200

    Fixed font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
         <Html className="">
           <Head>
               <title>MedicalOne RIS</title>
-              <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'/>
+              <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'/>
 
           </Head>
           <body>
