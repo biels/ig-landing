f621c2063759bfb3b9a00b1ca0507e52efdedbc6
Amiguet <amiguet06@gmail.com>
2021-09-12 23:23:03 +0200

    Update

--- lang/en-us.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    'section1.title':`<div>
-        Alef Tav Soft, the new generation management software for Hospitals and HealthCare Centers
-        Alef Tav Soft is the advanced solution that adapts to all your needs for you to easily manage your patients  administrative and healthcare information – anytime, anywhere.
-    </div>`,
-    'section1.description': `<div>
-        Alef Tav Soft is the advanced solution that adapts to all your needs for you to easily manage your patients  administrative and healthcare information – anytime, anywhere.
-    </div>`,
-}
